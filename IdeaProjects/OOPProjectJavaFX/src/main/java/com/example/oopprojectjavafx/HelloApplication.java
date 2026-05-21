8c566287ff9d46cd1dd10dc7ef99a798d6e0f258
Awab <awabmosama@gmail.com>
2026-05-21 18:07:36 +0500

    Starting Point

---
+++ IdeaProjects/OOPProjectJavaFX/src/main/java/com/example/oopprojectjavafx/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.example.oopprojectjavafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HelloApplication.class.getResource("/com/example/oopprojectjavafx/hello-view.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+
+        String CSS = HelloApplication.class.getResource("/com/example/oopprojectjavafx/style.css").toExternalForm();
+        scene.getStylesheets().add(CSS);
+
+        stage.setTitle("Logix Arcade");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ IdeaProjects/OOPProjectJavaFX/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.oopprojectjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.oopprojectjavafx to javafx.fxml;
+    exports com.example.oopprojectjavafx;
+}
